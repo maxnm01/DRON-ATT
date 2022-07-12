7162763c468f5776f36f1c67b9ab552f78bc94dc
maxnm01 <87444587+maxnm01@users.noreply.github.com>
2022-07-12 10:24:30 -0500

    Update App_dash.py
    Se agregaron algunos filtros con tags de HTML.

--- App_dash.py
+++ App_dash.py
@@ -16,8 +16,16 @@ import plotly.express as px
 import pandas as pd
 import pymysql
 from datetime import datetime
+from datetime import date
 import numpy as np
 import copy
+
+# Access to the RDS Database 
+Host = "Hostname"
+User = "Username"
+ID = "ID"
+Password = "password"
+Port = 
 
 #
 DB = pymysql.connect(host = Host, password = Password, user = User, port = Port)
@@ -31,20 +39,31 @@ df = pd.read_sql(SQL2,DB)
 # Order df by index 
 df = df.sort_index(ascending = False)
 
+#Fix 5G Innovation Lab location
+cdmx = [19.390519,-99.4238064]
+
 # Create map to show position
-m = folium.Map(location=[df.iloc[0]['Latitude'],df.iloc[0]['Longitude']], zoom_start=18)
+m = folium.Map(cdmx, zoom_start=5)
 
 # Add markers
-tooltip = "Haz clic"
-folium.Marker([df.iloc[0]['Latitude'],df.iloc[0]['Longitude']], popup="<i>Estoy aqui</>",  
-              icon=folium.Icon(color='red', icon='info-sign')).add_to(m)
+#tooltip = "Haz clic"
+#Add another locations
+for i in range(0,len(df)):
+    folium.Marker([df.iloc[i]['Latitude'],df.iloc[i]['Longitude']], 
+                  popup=df.iloc[i]["Phone_Number"]).add_to(m)
 
+#icon=folium.Icon(color='red', icon='info-sign')
 # Save map
 m.save('Geolocalización.html')
 
+#Set list of numbers
+options = []
+for value in np.unique(list(df["Phone_Number"])):
+    options.append({"label" : value, "value" : value})
+
 # Create the Dash app
 #app = dash.Dash(__name__)
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(external_stylesheets=[dbc.themes.CERULEAN])
 
 # the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE = {
@@ -52,47 +71,89 @@ SIDEBAR_STYLE = {
     "top": 0,
     "left": 0,
     "bottom": 0,
-    "width": "16rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
+    "width": "20rem",
+    "padding": "1rem 1rem",
+    "background-color": "#1e1e1e"
+    
 }
 
-# the styles for the main content position it to the right of the sidebar and
-# add some padding.
-#CONTENT_STYLE = {
-#    "margin-left": "18rem",
-#    "margin-right": "2rem",
-#    "padding": "2rem 1rem",
-#}
 
 sidebar = html.Div(
-    [
-        html.H2("Sidebar", className="display-4"),
+    [   
+        html.H1("DRON - 911",
+                className="navbar-dark bg-dark text-white p-3 mb-2 text-center",
+                style={'textAlign': 'center', 'top':'-5'}),     
+        html.H4("Seleccionar fecha", style={'textAlign': 'center'}),
+        dcc.DatePickerRange(initial_visible_month=datetime.now(),
+                            start_date = date(2022,7,1),
+                            end_date = date(2022,7,6),
+                             style={'margin':'0 auto', 'display':'inline-block'}),
+        
         html.Hr(),
-        html.P(
-            "Filters", className="lead"
-        ),
-        dbc.Nav(
-            [
-                dbc.NavLink("Home", href="/", active="exact")
-            ],
-            vertical=True,
-            pills=True,
-        ),
+        html.H4("Seleccionar Estado", style={'textAlign': 'center'}),
+        dcc.Dropdown(id='state_dd',
+        options=[
+            {'label':'Aguascalientes', 'value':'AGS'},
+            {'label':'Baja California', 'value':'BC'},
+            {'label':'Baja California Sur', 'value':'BCS'},
+            {'label':'Campeche', 'value':'CAMP'},
+            {'label':'Coahuila', 'value':'COAH'},
+            {'label':'Colima', 'value':'COL'},
+            {'label':'Chiapas', 'value':'CHIS'},
+            {'label':'Chihuahua', 'value':'CHIH'},
+            {'label':'Ciudad de México', 'value':'CDMX'},
+            {'label':'Durango', 'value':'DGO'},
+            {'label':'Guanajuato', 'value':'GTO'},
+            {'label':'Guerrero', 'value':'GRO'},
+            {'label':'Hidalgo', 'value':'HGO'},
+            {'label':'Jalisco', 'value':'JAL'},
+            {'label':'Estado de México', 'value':'MEX'},
+            {'label':'Michoacán', 'value':'MICH'},
+            {'label':'Morelos', 'value':'MOR'},
+            {'label':'Nayarit', 'value':'NAY'},
+            {'label':'Nuevo León', 'value':'NL'},
+            {'label':'Oaxaca', 'value':'OAX'},
+            {'label':'Puebla', 'value':'PUE'},
+            {'label':'Querétaro', 'value':'QRO'},
+            {'label':'Quintana Roo', 'value':'QROO'},
+            {'label':'San Luis Potosí', 'value':'SLP'},
+            {'label':'Sinaloa', 'value':'SIN'},
+            {'label':'Sonora', 'value':'SON'},
+            {'label':'Tabasco', 'value':'TAB'},
+            {'label':'Tamaulipas', 'value':'TAMPS'},
+            {'label':'Tlaxcala', 'value':'TLAX'},
+            {'label':'Veracruz', 'value':'VER'},
+            {'label':'Yucatán', 'value':'YUC'},
+            {'label':'Zacatecas', 'value':'ZAC'}],
+            style={'width':'200px', 'margin':'0 auto'}),
+        
+        html.Hr(),
+        html.H4("Seleccionar número", style={'textAlign': 'center'}),
+        dcc.Dropdown(id='cell_dd',
+        options = options,
+            style={'width':'200px', 'margin':'0 auto'})
+        
+        
     ],
     style=SIDEBAR_STYLE,
 )
-
-#content = html.Div(id="page-content", style=CONTENT_STYLE)
 
 #app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
 # Set up the layout using an overall div
 app.layout = html.Div(children=[
     sidebar,
-    html.H1(children='Localization Tracking'),
+    html.H1(children='Emergency Location Tracking',
+            className="navbar-dark bg-dark text-white p-4 mb-2 text-center",
+            style={"margin-left": "330px",'textAlign': 'center'}),
     html.Iframe(id = 'map', srcDoc = open('Geolocalización.html', 'r').read(), 
-                width = '100%', height = '600')
+                width = '100%', height = '400', 
+                style={
+                        "display": "inline-block",
+                        "width": "75%",
+                        "margin-left": "330px",
+                        "verticalAlign": "top"
+                    })
 ])
 
 
